81d86e538ceb7c884e059a1dc1e1f9d86e8dfe80
mokori4242 <mokori4242@gmail.com>
2023-09-29 15:17:55 +0900

    [add]第1章:最初の例~スタート地点~

---
+++ test/invoice.test.js
@@ -0,0 +1,42 @@
+import { statement } from '../invoice';
+
+describe('statement', () => {
+    const plays = {
+        "hamlet": { "name": "Hamlet", "type": "tragedy" },
+        "as-like": { "name": "As You Like it", "type": "comedy" },
+        "othello": { "name": "Othello", "type": "tragedy" }
+    };
+
+    const invoices = [
+        {
+            "customer": "BigCo",
+            "performances": [
+                {
+                    "playID": "hamlet",
+                    "audience": 55
+                },
+                {
+                    "playID": "as-like",
+                    "audience": 35
+                },
+                {
+                    "playID": "othello",
+                    "audience": 40
+                }
+            ]
+        }
+    ];
+
+    test('リファクタテスト', () => {
+        const invoice = invoices[0];
+        const result = statement(invoice, plays);
+
+        console.log(result);
+        expect(result).toContain("Statement for BigCo");
+        expect(result).toContain("Hamlet: $650.00 (55 seats)");
+        expect(result).toContain("As You Like it: $580.00 (35 seats)");
+        expect(result).toContain("Othello: $500.00 (40 seats)");
+        expect(result).toContain("Amount owed is $1,730.00");
+        expect(result).toContain("You earned 47 credits");
+    });
+});
