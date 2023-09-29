1985810e7addb4005cbe5430f32f53e51e7ba0c4
mokori4242 <mokori4242@gmail.com>
2023-09-29 19:16:32 +0900

    [update]statementのplayをplayFor()にしてインライン化

--- invoice.js
+++ invoice.js
@@ -9,12 +9,11 @@ export function statement(invoice, plays) {
         }).format;
 
     for (let perf of invoice.performances) {
-        const play = playFor(perf);
-        let thisAmount = amountFor(perf, play)
+        let thisAmount = amountFor(perf, playFor(perf))
 
         volumeCredits += Math.max(perf.audience - 30, 0);
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+        result += ` ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
         totalAmount += thisAmount;
     }
 
