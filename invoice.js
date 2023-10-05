283795d797df78373324f708b9f52e5d1f66f228
mokori4242 <mokori4242@gmail.com>
2023-10-06 08:50:50 +0900

    [update]formatという関数名だと曖昧すぎるのでusdに命名変更し重複していた100で割る処理を関数に移す

--- invoice.js
+++ invoice.js
@@ -5,11 +5,11 @@ export function statement(invoice, plays) {
 
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
-        result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
 
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
+    result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 
@@ -46,12 +46,12 @@ export function statement(invoice, plays) {
         return result;
     }
 
-    function format(aNumber) {
+    function usd(aNumber) {
         return new Intl.NumberFormat("en-US",
             {
                 style: "currency", currency: "USD",
                 minimumFrationDigits: 2
-            }).format(aNumber);
+            }).format(aNumber/100);
     }
 }
 
