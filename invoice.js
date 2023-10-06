e04f40cb172a9d178d1e1c48d6dd3b8a914f3e25
mokori4242 <mokori4242@gmail.com>
2023-10-06 14:54:53 +0900

    [update]performance(公演)も中間オブジェクトに追加。これによりrenderPlainTextのinvoiceパラメータが不要になる

--- invoice.js
+++ invoice.js
@@ -1,13 +1,14 @@
-export function statement(invoice, plays) {
+export function statement(data, plays) {
     const statementData = {};
-    statementData.customer = invoice.customer;
-    return renderPlainText(statementData, invoice, plays);
+    statementData.customer = data.customer;
+    statementData.performances = data.performances;
+    return renderPlainText(statementData, plays);
 }
 
-function renderPlainText(data, invoice, plays) {
+function renderPlainText(data, plays) {
     let result = `Statement for ${data.customer}\n`;
 
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
     result += `Amount owed is ${usd(totalAmount())}\n`;
@@ -42,7 +43,7 @@ function renderPlainText(data, invoice, plays) {
 
     function totalAmount() {
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += amountFor(perf);
         }
         return result;
@@ -57,7 +58,7 @@ function renderPlainText(data, invoice, plays) {
 
     function totalVolumeCredits() {
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += volumeCreditsFor(perf);
         }
         return result;
