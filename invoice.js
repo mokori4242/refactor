bd90cd7d6e3b5868420b2dde3e6c5dc5191f1bd5
mokori4242 <mokori4242@gmail.com>
2023-10-06 09:00:11 +0900

    [update]変数の値の集計ループを分離させる

--- invoice.js
+++ invoice.js
@@ -4,9 +4,11 @@ export function statement(invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
 
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
+    }
+    for (let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf);
     }
 
     result += `Amount owed is ${usd(totalAmount)}\n`;
