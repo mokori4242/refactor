31417119074fbe1590a9b7f535aa918d3e88b846
mokori4242 <mokori4242@gmail.com>
2023-10-06 19:20:14 +0900

    [update]合計を計算する関数にパイプラインによるループの置き換えを適用

--- invoice.js
+++ invoice.js
@@ -48,19 +48,13 @@ export function statement(invoice, plays) {
     }
 
     function totalAmount(data) {
-        let result = 0;
-        for (let perf of data.performances) {
-            result += perf.amount;
-        }
-        return result;
+        return data.performances
+            .reduce((total, p) => total + p.amount, 0)
     }
 
     function totalVolumeCredits(data) {
-        let result = 0;
-        for (let perf of data.performances) {
-            result += perf.volumeCredits;
-        }
-        return result;
+        return data.performances
+            .reduce((total, p) => total + p.volumeCredits, 0)
     }
 }
 
@@ -79,7 +73,7 @@ function renderPlainText(data) {
             {
                 style: "currency", currency: "USD",
                 minimumFrationDigits: 2
-            }).format(aNumber/100);
+            }).format(aNumber / 100);
     }
 }
 
