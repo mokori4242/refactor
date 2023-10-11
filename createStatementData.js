5b08e1d8de3b7f31ce0d375a4435a3cfc13e9c38
mokori4242 <mokori4242@gmail.com>
2023-10-12 08:36:33 +0900

    [add]喜劇のサブクラスを追加

--- createStatementData.js
+++ createStatementData.js
@@ -44,6 +44,17 @@ class TragedyCalculator extends PerformanceCalculator {
     }
 }
 
+class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        return result;
+    }
+}
+
 class PerformanceCalculator {
     constructor(aPerformance, aPlay) {
         this.performance = aPerformance;
