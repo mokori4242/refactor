ae1c7e1227929a41d7819f4c0969a18208620b19
mokori4242 <mokori4242@gmail.com>
2023-11-15 08:46:59 +0900

    [add]適切なJSONデータを生成する関数を追加

--- province.js
+++ province.js
@@ -7,8 +7,22 @@ class Province {
         this._price = doc.price;
         doc.producers.forEach(d => this.addProducer(new Producer(this, d)));
     }
+
     addProducer(arg) {
         this._producers.push(arg);
         this._totalProduction += arg.production;
     }
+}
+
+function sampleProvinceData() {
+    return {
+        name: "Asia",
+        producers: [
+            { name: "Byzantium", cost: 10, production: 9 },
+            { name: "Attalia", cost: 12, production: 10 },
+            { name: "Sinope", cost: 10, production: 6 },
+        ],
+        demand: 30,
+        price: 20
+    };
 }
