2c09b30d58d6cc8ec5037eedde9888ed04ce53ba
mokori4242 <mokori4242@gmail.com>
2023-11-17 10:24:16 +0900

    [add]Province:getterとsetterを追加

--- province.js
+++ province.js
@@ -8,6 +8,15 @@ class Province {
         doc.producers.forEach(d => this.addProducer(new Producer(this, d)));
     }
 
+    get name() { return this._name; }
+    get producers() { return this._producers; }
+    get totalProduction() { this._totalProduction = 0; }
+    set totalProduction(arg) { this._totalProduction = arg; }
+    get demand() { return this._demand; }
+    set demand(arg) { this._demand = parseInt(arg); }
+    get price() { return this._price; }
+    set price(arg) { this._price = parseInt(arg); }
+
     addProducer(arg) {
         this._producers.push(arg);
         this._totalProduction += arg.production;
