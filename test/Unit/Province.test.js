5302eea7f35b7284d812bb9234afca506990764c
mokori4242 <mokori4242@gmail.com>
2023-11-24 17:07:14 +0900

    [add]境界値の検査まで

--- test/Unit/Province.test.js
+++ test/Unit/Province.test.js
@@ -19,4 +19,43 @@ describe('province', function () {
         expect(asia.shortfall).toEqual(-6);
         expect(asia.profit).toEqual(292);
     });
+
+    it('zeroDemand', () => {
+        asia.demand = 0;
+        expect(asia.shortfall).toEqual(-25);
+        expect(asia.profit).toEqual(0);
+    });
+
+    it('negativeDemand', () => {
+        asia.demand = -1;
+        expect(asia.shortfall).toEqual(-26);
+        expect(asia.profit).toEqual(-10);
+    });
+
+    it('emptyStringDemand', () => {
+        asia.demand = "";
+        expect(asia.shortfall).toBeNaN();
+        expect(asia.profit).toBeNaN();
+    });
+});
+
+describe('no producers', function () {
+    let noProducers;
+    beforeEach(function () {
+        const data = {
+            name: noProducers,
+            producers: [],
+            demand: 30,
+            price: 20
+        };
+        noProducers = new Province(data);
+    });
+
+    it('shortfallTest', () => {
+        expect(noProducers.shortfall).toEqual(30);
+    });
+
+    it('profitTest', () => {
+        expect(noProducers.profit).toEqual(0);
+    });
 });
