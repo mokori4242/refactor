25cf1434a694224d54309a0aa1a123e5a782bf7f
mokori4242 <mokori4242@gmail.com>
2023-12-01 15:29:45 +0900

    [before]6:関数の抽出:例:スコープ外となる変数がない場合

--- 関数の抽出.js
+++ 関数の抽出.js
@@ -1,10 +1,21 @@
 function printOwing(invoice) {
-    printBanner();
-    let outstanding = calculateOutstanding();
-    printDetails(outstanding);
+    let outstanding = 0;
 
-    function printDetails(outstanding) {
-        console.log(`name: ${invoice.customer}`);
-        console.log(`amount: ${outstanding}`);
+    console.log("***********************");
+    console.log("**** Customer Owes ****");
+    console.log("***********************");
+
+    // 未払金の計算 calculate outstanding
+    for (const o of invoice.orders) {
+        outstanding += o.amount;
     }
+
+    // 締め日の記録 record due date
+    const today = Clock.today;
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+
+    // 明細の印字 print details
+    console.log(`name: ${invoice.customer}`);
+    console.log(`amount: ${outstanding}`);
+    console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
 }
