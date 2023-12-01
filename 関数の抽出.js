c40364db5c7acedc50913fd48d634e106cb45007
mokori4242 <mokori4242@gmail.com>
2023-12-01 15:47:54 +0900

    [after]6:関数の抽出:例:ローカル変数を使用する場合

--- 関数の抽出.js
+++ 関数の抽出.js
@@ -8,17 +8,23 @@ function printOwing(invoice) {
         outstanding += o.amount;
     }
 
-    // 締め日の記録 record due date
-    const today = Clock.today;
-    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
-
-    // 明細の印字 print details
-    console.log(`name: ${invoice.customer}`);
-    console.log(`amount: ${outstanding}`);
-    console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+    recordDueDate(invoice);
+    printDetails(invoice, outstanding);
 }
+
 function printBanner() {
     console.log("***********************");
     console.log("**** Customer Owes ****");
     console.log("***********************");
+}
+
+function recordDueDate(invoice) {
+    const today = Clock.today;
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+}
+
+function printDetails(invoice, outstanding) {
+    console.log(`name: ${invoice.customer}`);
+    console.log(`amount: ${outstanding}`);
+    console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
 }
