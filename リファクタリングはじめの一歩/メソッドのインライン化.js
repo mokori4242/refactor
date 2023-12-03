4715419e8a156e7022f964f4cf6f07483286abfb
mokori4242 <mokori4242@gmail.com>
2023-12-04 08:33:50 +0900

    [before]6:メソッドのインライン化:例:更に入り組んだ場合

--- リファクタリングはじめの一歩/メソッドのインライン化.js
+++ リファクタリングはじめの一歩/メソッドのインライン化.js
@@ -1,3 +1,9 @@
-function getRating(aDrive) {
-    return aDrive.numberOfLateDeliveries > 5 ? 2 : 1;
+function reportLines(aCustomer) {
+  const lines = []
+  gatherCustomerData(lines, aCustomer)
+  return lines
+}
+function gatherCustomerData(out, aCustomer) {
+  out.push(['name', aCustomer.name])
+  out.push(['location', aCustomer.location])
 }
