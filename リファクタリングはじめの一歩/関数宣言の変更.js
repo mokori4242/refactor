83934ccc7f4d8017993cf8f8a92acfce27749b03
mokori4242 <mokori4242@gmail.com>
2023-12-15 10:47:18 +0900

    [before]6:関数宣言の変更:例:パラメータの追加

--- リファクタリングはじめの一歩/関数宣言の変更.js
+++ リファクタリングはじめの一歩/関数宣言の変更.js
@@ -1,6 +1,5 @@
-function circum(radius) {
-    return circumference(radius);
-}
-function circumference(radius) {
-    return 2 * Math.PI * radius;
+class Book {
+    addReservation(customer) {
+        this._reservations.push(customer);
+    }
 }
