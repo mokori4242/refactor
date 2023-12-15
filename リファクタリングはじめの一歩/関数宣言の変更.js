ecb014d1aa729a81344fc251eccc39070c9da34d
mokori4242 <mokori4242@gmail.com>
2023-12-15 12:13:43 +0900

    [before]6:関数宣言の変更:例:パラメータをプロパティに変更する

--- リファクタリングはじめの一歩/関数宣言の変更.js
+++ リファクタリングはじめの一歩/関数宣言の変更.js
@@ -1,10 +1,3 @@
-class Book {
-    addReservation(customer) {
-        this.zz_addReservation(customer, false);
-    }
-
-    zz_addReservation(customer, isPriority) {
-        assert(isPriority === true || isPriority === false);
-        this._reservations.push(customer);
-    }
+function inNewEngland(aCustomer) {
+  return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(aCustomer.address.state)
 }
