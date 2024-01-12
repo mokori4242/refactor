2006b06297b286f49aad3803eb0a3df44a661757
mokori4242 <mokori4242@gmail.com>
2024-01-12 16:03:00 +0900

    [update]6:パラメータオブジェクトの導入

--- リファクタリングはじめの一歩/パラメータオブジェクトの導入.js
+++ リファクタリングはじめの一歩/パラメータオブジェクトの導入.js
@@ -1,3 +1,3 @@
-function amountInvoiced(startDate, endDate) { ... }
-function amountReceived(startDate, endDate) { ... }
-function amountOverdue(startDate, endDate) { ... }
+function amountInvoiced(aDateRange) { ... }
+function amountReceived(aDateRange) { ... }
+function amountOverdue(aDateRange) { ... }
