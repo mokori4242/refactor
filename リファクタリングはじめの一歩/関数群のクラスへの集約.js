21881d3ec612949acfe44c1f9537f5049b319d89
mokori4242 <mokori4242@gmail.com>
2024-01-17 08:34:00 +0900

    [after]6:関数群のクラスへの集約:例

--- リファクタリングはじめの一歩/関数群のクラスへの集約.js
+++ リファクタリングはじめの一歩/関数群のクラスへの集約.js
@@ -1,18 +1,37 @@
+class Reading {
+    constructor(data) {
+        this._customer = data.customer;
+        this._quantity = data.quantity;
+        this._month = data.month;
+        this._year = data.year;
+    }
+    get customer() { return this._customer; }
+    get quantity() { return this._quantity; }
+    get month() { return this._month; }
+    get year() { return this._year; }
+
+    get baseCharge() {
+        return baseRate(this.month, this.year) * this.quantity;
+    }
+
+    get taxableChargeFn() {
+        return Math.max(0, this.baseCharge - taxThreshold(this.year))
+    }
+}
+
 reading = { customer: "ivan", quantity: 10, month: 5, year: 2017 };
 
 // client1
-const aReading = acquireReading();
-const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+const rawReading = acquireReading();
+const aReading = new Reading(rawReading);
+const baseCharge = aReading.baseCharge;
 
 // client2
-const aReading = acquireReading();
-const base = (baseRate(aReading.month, aReading.year) * aReading.quantity);
-const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+const rawReading = acquireReading();
+const aReading = new Reading(rawReading);
+const taxableCharge = aReading.taxableChargeFn;
 
 // client3
-const aReading = acquireReading();
-const basicChargeAmount = calculateBaseCharge(aReading);
-
-function calculateBaseCharge(aReading) {
-    return baseRate(aReading.month, aReading.year) * aReading.quantity;
-}
+const rawReading = acquireReading();
+const aReading = new Reading(rawReading);
+const basicChargeAmount = aReading.baseCharge;
