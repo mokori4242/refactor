b0de1f5ee0f97c9e81174d0877470a6a9d8bae57
mokori4242 <mokori4242@gmail.com>
2024-01-19 14:57:20 +0900

    [update]6:フェーズの分離の例

--- リファクタリングはじめの一歩/フェーズの分離.js
+++ リファクタリングはじめの一歩/フェーズの分離.js
@@ -1,13 +1,14 @@
-const orderRecord = parseOrder(order);
-const orderPrice = price(orderRecord, priceList);
-
-function parseOrder(aString) {
-    const values = aString.split(/\s+/);
-    return ({
-        productID: values[0],
-        quantity: parseInt(values[1])
-    });
+function priceOrder(product, quantity, shippingMethod) {
+    const priceData = calculationPricingData(product, quantity);
+    return applyShipping(priceData, shippingMethod);
 }
-function price(order, priceList) {
-    return order.quantity * priceList[order.productID];
+function calculationPricingData(product, quantity) {
+    const basePrice = product.basePrice * quantity;
+    const discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
+    return { basePrice: basePrice, quantity: quantity, discount: discount};
+}
+function applyShipping(priceData, shippingMethod) {
+    const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold) ? shippingMethod.discountedFee : shippingMethod.feePerCase;
+    const shippingCost = priceData.quantity * shippingPerCase;
+    return priceData.basePrice - priceData.discount + shippingCost;
 }
