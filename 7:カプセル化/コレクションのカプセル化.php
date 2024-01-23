415a0fb6abbc918006775ee71059e935dcefb5e4
mokori4242 <mokori4242@gmail.com>
2024-01-24 08:05:04 +0900

    [update]コレクションのカプセル化

--- 7:カプセル化/コレクションのカプセル化.php
+++ 7:カプセル化/コレクションのカプセル化.php
@@ -2,16 +2,30 @@
 // 翻訳とコードがわかりづらすぎたので(とりあえず)ここだけ普段使いのPHPで…
 class Order
 {
-    public $products = [];
+    private $products = [];
 
     public function addProduct($product)
     {
         $this->products[] = $product;
     }
+
+    public function getProducts()
+    {
+        return $this->products;
+    }
+
+    // 任意で、特定のプロダクトを取り除くメソッドを追加することもできます
+    public function removeProduct($product)
+    {
+        $this->products = array_filter($this->products, function($p) use ($product) {
+            return $p !== $product;
+        });
+    }
 }
 
 // 使用例
 $order = new Order();
-$order->products[] = "Apple";
-$order->products[] = "Banana";
+$order->addProduct("Apple");
+$order->addProduct("Banana");
+$products = $order->getProducts();
 
