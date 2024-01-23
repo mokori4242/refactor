8206e42cc9de6346b8104550ab280385aa359faa
mokori4242 <mokori4242@gmail.com>
2024-01-24 08:53:11 +0900

    [after]オブジェクトによるプリミティブの置き換え:例

--- 7:カプセル化/オブジェクトによるプリミティブの置き換え.php
+++ 7:カプセル化/オブジェクトによるプリミティブの置き換え.php
@@ -1,18 +1,45 @@
 <?php
+
+class PhoneNumber
+{
+    private $number;
+
+    public function __construct($number)
+    {
+        $this->number = $number;
+    }
+
+    // 電話番号のフォーマットを行うメソッド
+    public function format()
+    {
+        // 電話番号を特定のフォーマットに整形するロジック
+        return str_replace("-", "", $this->number);
+    }
+
+    // その他の電話番号に関連するメソッド
+}
+
 class Person
 {
-    public $phoneNumber; // 電話番号を単なる文字列として保持
+    private $phoneNumber; // PhoneNumberオブジェクトに置き換え
 
-    public function __construct($phoneNumber)
+    public function __construct(PhoneNumber $phoneNumber)
     {
         $this->phoneNumber = $phoneNumber;
+    }
+
+    public function getFormattedPhoneNumber()
+    {
+        return $this->phoneNumber->format();
     }
 
     // ...
 }
 
-// 電話番号を直接文字列として扱う
-$person = new Person("123-456-7890");
 
-// 電話番号を取得
-echo $person->phoneNumber;
+// PhoneNumberオブジェクトを作成し、それをPersonに渡す
+$phoneNumber = new PhoneNumber("123-456-7890");
+$person = new Person($phoneNumber);
+
+// フォーマットされた電話番号を取得
+echo $person->getFormattedPhoneNumber();
