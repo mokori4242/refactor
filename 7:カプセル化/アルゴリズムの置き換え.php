743847092f089fef39aea58fbcc3b8b8c1f3e6ef
mokori4242 <mokori4242@gmail.com>
2024-01-26 11:03:51 +0900

    [after]アルゴリズムの置き換え

--- 7:カプセル化/アルゴリズムの置き換え.php
+++ 7:カプセル化/アルゴリズムの置き換え.php
@@ -1,16 +1,9 @@
 <?php
-
 class StringProcessor
 {
     public function countLetter($string, $letter)
     {
-        $count = 0;
-        for ($i = 0; $i < strlen($string); $i++) {
-            if ($string[$i] === $letter) {
-                $count++;
-            }
-        }
-        return $count;
+        return substr_count($string, $letter);
     }
 }
 
