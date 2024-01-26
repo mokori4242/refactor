4b45d2a609940fa03fa38ebf266803a7ce18e837
mokori4242 <mokori4242@gmail.com>
2024-01-26 18:12:37 +0900

    [after]8:フィールドの移動

--- 8:特製の移動/フィールドの移動.php
+++ 8:特製の移動/フィールドの移動.php
@@ -3,19 +3,17 @@
 class Customer
 {
     private $name;
-    private $discountRate;
     private $contract;
 
     public function __construct($name, $discountRate)
     {
         $this->name = $name;
-        $this->discountRate = $discountRate;
-        $this->contract = new CustomerContract(date("Y-m-d"));
+        $this->contract = new CustomerContract(date("Y-m-d"), $discountRate);
     }
 
     public function getDiscountRate()
     {
-        return $this->discountRate;
+        return $this->contract->getDiscountRate();
     }
 
     // その他のメソッド
@@ -24,10 +22,17 @@ class Customer
 class CustomerContract
 {
     private $startDate;
+    private $discountRate;
 
-    public function __construct($startDate)
+    public function __construct($startDate, $discountRate)
     {
         $this->startDate = $startDate;
+        $this->discountRate = $discountRate;
+    }
+
+    public function getDiscountRate()
+    {
+        return $this->discountRate;
     }
 
     // その他のメソッド
