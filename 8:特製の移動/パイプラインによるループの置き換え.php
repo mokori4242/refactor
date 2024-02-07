5812f725d1b94b13c3d1531051802c978b8da70f
mokori4242 <mokori4242@gmail.com>
2024-02-08 08:46:39 +0900

    [after]8:パイプラインによるループの置き換え

--- 8:特製の移動/パイプラインによるループの置き換え.php
+++ 8:特製の移動/パイプラインによるループの置き換え.php
@@ -1,25 +1,15 @@
 <?php
 
 $numbers = [1, 2, 3, 4, 5];
-$doubledNumbers = []; // 数値を2倍にした結果を格納する配列
-$evenNumbers = []; // 偶数のみを格納する配列
-$total = 0; // 偶数の合計値
 
-// まず、各数値を2倍にする
-foreach ($numbers as $number) {
-    $doubledNumbers[] = $number * 2;
-}
+// 各数字を2倍にして、偶数のみを抽出し、その合計を計算する
+$total = array_reduce(
+    array_filter(
+        array_map(function ($n) { return $n * 2; }, $numbers),
+        function ($n) { return $n % 2 === 0; }
+    ),
+    function ($carry, $n) { return $carry + $n; },
+    0
+);
 
-// 次に、2倍にした数値の中から偶数のみを抽出する
-foreach ($doubledNumbers as $doubledNumber) {
-    if ($doubledNumber % 2 === 0) {
-        $evenNumbers[] = $doubledNumber;
-    }
-}
-
-// 最後に、偶数の合計を計算する
-foreach ($evenNumbers as $evenNumber) {
-    $total += $evenNumber;
-}
-
-echo $total; // 出力: 20 (4 + 8 + 8)
+echo $total; // 出力: 30(2 + 4 + 6 + 8 + 10)
