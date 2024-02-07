d564c248eb069694e94f3f36023cc89823dc0757
mokori4242 <mokori4242@gmail.com>
2024-02-08 08:18:53 +0900

    [after]8:ループの分離

--- 8:特製の移動/ループの分離.php
+++ 8:特製の移動/ループの分離.php
@@ -1,16 +1,12 @@
 <?php
 
-$products = [
-    ["name" => "商品A", "price" => 1000, "inStock" => true],
-    ["name" => "商品B", "price" => 2000, "inStock" => false],
-    // その他の商品...
-];
-
 $totalPrice = 0;
-$outOfStock = [];
-
 foreach ($products as $product) {
     $totalPrice += $product["price"];
+}
+
+$outOfStock = [];
+foreach ($products as $product) {
     if (!$product["inStock"]) {
         $outOfStock[] = $product["name"];
     }
