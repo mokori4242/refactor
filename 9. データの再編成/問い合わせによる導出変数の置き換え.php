1eac02bdb7a7dc0aa9df8ed8410a24d993b295ee
mokori4242 <mokori4242@gmail.com>
2024-02-09 08:50:24 +0900

    [after]9:問い合わせによる導出変数の置き換え

--- 9. データの再編成/問い合わせによる導出変数の置き換え.php
+++ 9. データの再編成/問い合わせによる導出変数の置き換え.php
@@ -3,11 +3,24 @@
 class Cart
 {
     private $items = [];
-    public $totalPrice = 0;
 
     public function addItem($item)
     {
         $this->items[] = $item;
-        $this->totalPrice += $item->price;
+    }
+
+    public function getTotalPrice()
+    {
+        $totalPrice = 0;
+        foreach ($this->items as $item) {
+            $totalPrice += $item->price;
+        }
+        return $totalPrice;
     }
 }
+
+// クライアントコード
+$cart = new Cart();
+$cart->addItem(new Item("Apple", 100));
+$cart->addItem(new Item("Banana", 50));
+echo "Total Price: " . $cart->getTotalPrice();
