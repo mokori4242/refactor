e3833ec5ff3db12c893efce1f0b65d29ef075bcc
mokori4242 <mokori4242@gmail.com>
2024-02-09 09:08:30 +0900

    [after]9:参照から値への変更

--- 9. データの再編成/参照から値への変更.php
+++ 9. データの再編成/参照から値への変更.php
@@ -2,9 +2,9 @@
 
 class Color
 {
-    public $red;
-    public $green;
-    public $blue;
+    private $red;
+    private $green;
+    private $blue;
 
     public function __construct($red, $green, $blue)
     {
@@ -12,8 +12,20 @@ class Color
         $this->green = $green;
         $this->blue = $blue;
     }
+
+    public function equals(Color $other)
+    {
+        return $this->red === $other->red
+            && $this->green === $other->green
+            && $this->blue === $other->blue;
+    }
 }
 
 $color1 = new Color(255, 255, 255);
-$color2 = $color1;
-$color2->red = 128; // $color1も変更される
+$color2 = new Color(255, 255, 255);
+
+if ($color1->equals($color2)) {
+    echo "Color1 and Color2 are equal.";
+} else {
+    echo "Color1 and Color2 are not equal.";
+}
