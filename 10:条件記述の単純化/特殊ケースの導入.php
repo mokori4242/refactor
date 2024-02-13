1b6ff717cf813ad5b61ae648acbe49287a696ce3
mokori4242 <mokori4242@gmail.com>
2024-02-14 08:39:20 +0900

    [after]10:特殊ケースの導入

--- 10:条件記述の単純化/特殊ケースの導入.php
+++ 10:条件記述の単純化/特殊ケースの導入.php
@@ -1,9 +1,35 @@
 <?php
 
-function getCustomerName($customer)
+class Customer
 {
-    if ($customer == null) {
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
+class UnknownCustomer extends Customer
+{
+    public function getName()
+    {
         return "Unknown";
     }
+}
+
+function getCustomerName($customer)
+{
     return $customer->getName();
 }
+
+function getCustomer($id)
+{
+    $customer = CustomerRepository::findById($id);
+    if ($customer == null) {
+        return new UnknownCustomer();
+    }
+    return $customer;
+}
+
+//　クライアントコード
+$customer = getCustomer($id);
+echo getCustomerName($customer);
