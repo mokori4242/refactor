29c39aa616b2e5567b08697322d62260cec1ce5c
mokori4242 <mokori4242@gmail.com>
2024-02-14 09:02:08 +0900

    [after]10:アサーションの導入

--- 10:条件記述の単純化/アサーションの導入.php
+++ 10:条件記述の単純化/アサーションの導入.php
@@ -2,6 +2,9 @@
 
 function calculateDiscount($customer)
 {
+    // アサーション: 顧客の年数は負の数であってはならない
+    assert($customer->years >= 0, 'Customer years must not be negative.');
+
     // 割引率の計算
     $discount = 0.0;
 
@@ -11,3 +14,8 @@ function calculateDiscount($customer)
     }
     return $discount;
 }
+
+// クライアントコード
+$customer = new Customer();
+$customer->years = 6;
+echo calculateDiscount($customer); // アサーションを満たす場合、割引率を計算
