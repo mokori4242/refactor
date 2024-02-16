b727c66f0aed210b636fcff41833325ac4d58f6e
mokori4242 <mokori4242@gmail.com>
2024-02-16 10:17:31 +0900

    [after]11:フラグパラメータの削除

--- 11:APIのリファクタリング/フラグパラメータの削除.php
+++ 11:APIのリファクタリング/フラグパラメータの削除.php
@@ -1,10 +1,15 @@
 <?php
 
-function updateProduct($product, $isSale)
+function updateProductSalePrice($product)
 {
-    if ($isSale) {
-        // セール価格を更新
-    } else {
-        // 通常価格を更新
-    }
+    // セール価格を更新
 }
+
+function updateProductRegularPrice($product)
+{
+    // 通常価格を更新
+}
+
+// クライアントコード
+updateProductSalePrice($product); // セール価格の更新の場合
+updateProductRegularPrice($product); // 通常価格の更新の場合
