b828a255c9e488e04568e29472bb7e5391c410f6
mokori4242 <mokori4242@gmail.com>
2024-02-16 09:52:58 +0900

    [before]11:パラメータによる関数の統合

---
+++ 11:APIのリファクタリング/パラメータによる関数の統合.php
@@ -0,0 +1,13 @@
+<?php
+
+function generateSalesReport($data)
+{
+    // 売上レポートの生成ロジック
+    return 'Sales Report:' . $data;
+}
+
+function generateEmployeeReport($data)
+{
+    // 従業員レポートの生成ロジック
+    return 'Employee Report:' . $data;
+}
