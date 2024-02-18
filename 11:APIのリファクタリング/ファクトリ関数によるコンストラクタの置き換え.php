3d9f7ba87bbac7f5577ff7377119b4bb9c6720ef
mokori4242 <mokori4242@gmail.com>
2024-02-19 08:50:09 +0900

    [after]11:ファクトリ関数によるコンストラクタの置き換え

--- 11:APIのリファクタリング/ファクトリ関数によるコンストラクタの置き換え.php
+++ 11:APIのリファクタリング/ファクトリ関数によるコンストラクタの置き換え.php
@@ -5,14 +5,25 @@ class Employee
     private $name;
     private $type;
 
-    public function __construct($name, $type)
+    private function __construct($name, $type)
     {
         $this->name = $name;
         $this->type = $type;
+    }
+
+    public static function createEngineer($name)
+    {
+        return new Employee($name, 'Engineer');
+    }
+
+    public static function createSalesman($name)
+    {
+        return new Employee($name, 'Salesman');
     }
 
     // nameとtypeのゲッターメソッドは省略
 }
 
 // クライアントコード
-$employee = new Employee("Coffee", "Beverage");
+$engineer = Employee::createEngineer("Mike");
+$salesman = Employee::createSalesman("Vince");
