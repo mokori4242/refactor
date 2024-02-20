1f77a02249a396e815bb9a93893276228574d4ab
mokori4242 <mokori4242@gmail.com>
2024-02-21 08:32:17 +0900

    [after]12:メソッドの引き上げ

--- 12:継承の取り扱い/メソッドの引き上げ.php
+++ 12:継承の取り扱い/メソッドの引き上げ.php
@@ -2,21 +2,25 @@
 
 class Vehicle
 {
-    // 共通のプロパティやメソッド
+    public function commonMethod()
+    {
+        echo '共通の振る舞い';
+    }
 }
 
 class Car extends Vehicle
 {
-    public function commonMethod()
-    {
-        echo '共通の振る舞い';
-    }
+    // 特有のプロパティやメソッド
 }
 
 class Truck extends Vehicle
 {
-    public function commonMethod()
-    {
-        echo '共通の振る舞い';
-    }
+    // 特有のプロパティやメソッド
 }
+
+// クライアントコード
+$car = new Car();
+$car->commonMethod(); // "共通の振る舞い" を出力
+
+$truck = new Truck();
+$truck->commonMethod(); // "共通の振る舞い" を出力
