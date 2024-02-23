986c1f2360083d7487828c774483e5cf05c31cab
mokori4242 <mokori4242@gmail.com>
2024-02-23 10:31:28 +0900

    [before]12:フィールドの押し下げ

---
+++ 12:継承の取り扱い/フィールドの押し下げ.php
@@ -0,0 +1,16 @@
+<?php
+
+class Vehicle
+{
+    protected $manufacturer;
+}
+
+class Car extends Vehicle
+{
+    // Car-specific behaviors
+}
+
+class Truck extends Vehicle
+{
+    // Truck-specific behaviors
+}
