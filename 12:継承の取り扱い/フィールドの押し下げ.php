c53030ef68b314eb65294214fe7e970b1363620b
mokori4242 <mokori4242@gmail.com>
2024-02-23 10:33:18 +0900

    [after]12:フィールドの押し下げ

--- 12:継承の取り扱い/フィールドの押し下げ.php
+++ 12:継承の取り扱い/フィールドの押し下げ.php
@@ -2,15 +2,24 @@
 
 class Vehicle
 {
-    protected $manufacturer;
+    // Common behaviors for all vehicles
 }
 
 class Car extends Vehicle
 {
+    protected $manufacturer;
     // Car-specific behaviors
 }
 
 class Truck extends Vehicle
 {
-    // Truck-specific behaviors
+    // Truck-specific behaviors remain unchanged
 }
+
+// クライアントコード
+$car = new Car();
+$car->manufacturer = "Toyota"; // これはCarクラスに定義されています。
+
+$truck = new Truck();
+// $truck->manufacturer = "Ford"; // Truckクラスでは`manufacturer`フィールドは定義されていません。
+
